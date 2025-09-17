38a79bcb442155f9cbf42d2ae171575248fa8b50
Bihai Sun <bihai.sun@monash.edu>
2025-09-18 01:10:01 +1000

    update

--- run/file_list_gen.jl
+++ run/file_list_gen.jl
@@ -1,9 +1,21 @@
 
 ##
-base_dir = "../../fields/time_resolved_run1_dt40_steps"
+base_dir = "../../rawdata_long_interval"
+
+function list_files_recursive(dir_path::AbstractString)
+    file_list = String[]
+    for (root, dirs, files) in walkdir(dir_path)
+        println("Directory: $root")
+
+        for f in files
+            push!(file_list, joinpath(root, f))
+        end
+    end
+    return file_list
+end
 
 
-raw_file_names = readdir(base_dir)
+raw_file_names = list_files_recursive(base_dir)
 
 # Find all files matching *.u.h5 pattern
 u_files = filter(name -> endswith(name, ".u.h5"), raw_file_names)
@@ -19,8 +31,7 @@ output_file = "files_list.txt"
 open(output_file, "w") do io
     for file in u_files
         # Create full path and strip .u.h5 extension
-        full_path = joinpath(base_dir, file)
-        name_without_ext = replace(full_path, ".u.h5" => "")
+        name_without_ext = replace(file, ".u.h5" => "")
         println(io, name_without_ext)
     end
 end
